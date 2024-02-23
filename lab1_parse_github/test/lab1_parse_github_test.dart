941189daac004328d145251193cc398dffeaf2a0
True._.Mary <truefalsemary@gmail.com>
2024-02-24 00:42:32 +0300

    test: test githubSearch request

--- lab1_parse_github/test/lab1_parse_github_test.dart
+++ lab1_parse_github/test/lab1_parse_github_test.dart
@@ -1,8 +1,24 @@
+import 'package:lab1_parse_github/github_client.dart';
 import 'package:lab1_parse_github/lab1_parse_github.dart';
+import 'package:lab1_parse_github/search_result_models.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  test('Test 200 request', () async {
+    final client = GithubClient();
+    final searchResult =
+        await client.search(username: 'truefalsemary', term: 'tech-front-new');
+    print(searchResult);
+
+    expect(searchResult, isA<SearchResult>());
+    expect(searchResult.items, isA<List<SearchResultItem>>());
+  });
+
+  test('Test Not Found request', () async {
+    final client = GithubClient();
+    expect(
+      () async => await client.search(username: '', term: ''),
+      throwsA(isA<SearchResultError>()),
+    );
   });
 }
